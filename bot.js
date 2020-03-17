87248a3e7b616a74179b7acbfe4cb7346a96b5f0
Isabela <belacoper@gmail.com>
2020-03-17 11:09:57 -0300

    wash your hands js bot

---
+++ bot.js
@@ -0,0 +1,36 @@
+var twit = require('twit'); // require the lib
+var config = require('./config.js'); // require the configurations
+const randomItem = require('random-item');
+var schedule = require('node-schedule');
+
+
+var Twitter = new twit(config); // pass the configuration to the twit
+
+var arr = [ 
+            'Hey, have you already washed your hands.',
+            'Coronavirus is not kidding, please wash your hands.',
+            'I dont know why you still going out, but please remember to wash your hands.',
+            'CORONA VIRUSSSS(Card B voice)!!! Wash your hands!!!!',
+            'Do you wanna play a game? Please, wash your hands.',
+            'Im here to remind you to wash yout hands :)',
+            'Once again: dont forget to wash your hands.',
+            'Time to wash your hands!',
+            'WASSSSHHH YOUR HANDS',
+            'Do me a favour: wash your hands!',
+            'Did you washed your hands today?',
+            'Doing nothing? Omg, please wash your hands.'
+                
+           ];
+
+var j = schedule.scheduleJob("*/30 * * * *", function(){
+
+    Twitter.post('statuses/update', 
+    { 
+        status: randomItem (arr) 
+    }, 
+    function(err, data, response) {
+        console.log(data)
+    });
+
+
+});   
---
+++ config.js
@@ -0,0 +1,11 @@
+//config.js
+/**twitter app config
+ * 
+ */
+
+module.exports = {
+  consumer_key: 'P36t5nDoOGvwWho1EJjcZ0jcm',
+  consumer_secret: 'YasNVVDCH7G6LyL4gDZB3BaDiU8gy68HWh9rmXAnt59FjcUwt7',
+  access_token: '1239898231896715265-avNC9QMZ3ktASUmwCRfpsMQdMIgH5H',
+  access_token_secret: 'aJUfRK4Gigiz3jX0ewxbIqBHGpwbjX0kDbUNVoz8PIOZ1'
+}
