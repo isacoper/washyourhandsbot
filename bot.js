ec62be30665dba13663e198c591a7609948d46dd
Isabela Correia <belacoper@gmail.com>
2020-03-27 12:11:47 -0300

    fixing commas

--- bot.js
+++ bot.js
@@ -19,10 +19,10 @@ var arr = [
             'WASSSSHHH YOUR HANDS',
             'Do me a favour: wash your hands!',
             'Did you wash your hands today?',
-            'Doing nothing? Omg, please wash your hands.'
-            'Wash your hands at least for 20 seconds and DO NOT LISTEN to Bolsonaro!'
-            'Please wash your hands and stay home.'
-            'There is no economy when people are dead, so please stay home and wash your hands.'
+            'Doing nothing? Omg, please wash your hands.',
+            'Wash your hands at least for 20 seconds and DO NOT LISTEN to Bolsonaro!',
+            'Please wash your hands and stay home.',
+            'There is no economy when people are dead, so please stay home and wash your hands.',
             'Wash your hands, take your vitamins, stay home if you can and ignore Brazilian president.'
                 
            ];
